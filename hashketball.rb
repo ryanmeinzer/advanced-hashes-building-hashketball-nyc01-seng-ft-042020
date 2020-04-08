278cb7c874a5c01467c71b2ca810a41e221695df
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-04-08 16:57:46 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,9 +4,27 @@ def game_hash
 
 hashketball_hash = {
 
-  :home => {}
+  :home = {
+    :team_name = ""
+    :colors = [teams-colors]
+    :players = [
+        {
+        :player_name = ""
+        :number = ""
+        :shoe = ""
+        :points = ""
+        :rebounds = ""
+        :assists = ""
+        :steals = ""
+        :blocks = ""
+        :slam_dunks = ""
+        }
+        {player2-stats}
+        {player3-stats}
+    ]
+  }
 
-  :away => {}
+  :away = {}
 
 }
 
